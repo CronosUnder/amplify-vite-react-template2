f69111fc08d48bdab17d7bbfe4245e253c1ae433
claudiomiranda <cronosunder@gmail.com>
2025-03-26 18:25:20 -0300

    feat: Add team and member relation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,8 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import {
+  a,
+  type ClientSchema,
+  defineData
+} from "@aws-amplify/backend";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -6,12 +10,52 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
+/*
+type Playlist @model {
+  id: ID!
+  name: String!
+  description: String!
+  owner: String
+  createdAt: String
+  posts: [PlaylistPost] @connection(name: "PlaylistPost")
+}
+type Post @model {
+  id: ID!
+  name: String!
+  description: String!
+  thumbnail: S3Object!
+  video: S3Object!
+  owner: String
+  createdAt: String
+  playlists: [PlaylistPost] @connection(name: "PostPlaylist")
+}
+type PlaylistPost @model {
+  id: ID!
+  createdAt: String
+  playlist: Playlist @connection(name: "PlaylistPost")
+  post: Post @connection(name: "PostPlaylist", sortField: "updatedAt")
+}
+*/
 const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
+  Member: a.model({
+    name: a.string().required(),
+    // 1. Create a reference field
+    teamId: a.id(),
+    // 2. Create a belongsTo relationship with the reference field
+    team: a.belongsTo('Team', 'teamId'),
+  }),
+
+  Team: a.model({
+    mantra: a.string().required(),
+    // 3. Create a hasMany relationship with the reference field
+    //    from the `Member`s model.
+    members: a.hasMany('Member', 'teamId'),
+  }),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -32,7 +76,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
